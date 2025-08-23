bf0aa4c6478fbe19acd6cbe7ad8940bda3a177dc
joeuser846@gmail.com <joeuser846@gmail.com>
2025-08-23 12:11:28 -0400

    Tune constants

--- config/lino_base_config.h
+++ config/lino_base_config.h
@@ -54,7 +54,7 @@ ROBOT ORIENTATION
 */
 
 //define your robot' specs here
-#define MOTOR_MAX_RPM 140                   // motor's max RPM          
+#define MOTOR_MAX_RPM 100                   // motor's max RPM          
 #define MAX_RPM_RATIO 0.85                  // max RPM allowed for each MAX_RPM_ALLOWED = MOTOR_MAX_RPM * MAX_RPM_RATIO          
 #define MOTOR_OPERATING_VOLTAGE 12          // motor's operating voltage (used to calculate max RPM)
 #define MOTOR_POWER_MAX_VOLTAGE 10          // max voltage of the motor's power source (used to calculate max RPM)
@@ -63,9 +63,9 @@ ROBOT ORIENTATION
 #define COUNTS_PER_REV2 144000              // wheel2 encoder's no of ticks per rev
 #define COUNTS_PER_REV3 144000              // wheel3 encoder's no of ticks per rev
 #define COUNTS_PER_REV4 144000              // wheel4 encoder's no of ticks per rev
-#define WHEEL_DIAMETER 0.152                // wheel's diameter in meters
-#define LR_WHEELS_DISTANCE 0.271            // distance between left and right wheels
-#define PWM_BITS 10                          // PWM Resolution of the microcontroller
+#define WHEEL_DIAMETER 0.05                 // wheel's diameter in meters (5cm)
+#define LR_WHEELS_DISTANCE 0.16             // distance between left and right wheels in meters (16cm)
+#define PWM_BITS 10                         // PWM Resolution of the microcontroller
 #define PWM_FREQUENCY 20000                 // PWM Frequency
 
 // INVERT ENCODER COUNTS
